eb9aed46717cefe558d1e26741d301e4684b5bcb
Antoine Lamielle <gh@0x010c.fr>
2018-10-22 18:02:27 -0700

    New command level to split the old manual mode from the futur live mode

--- llbot.py
+++ llbot.py
@@ -82,29 +82,12 @@ def get_records( query ):
 	return records
 
 
-# Main
-def main():
-	# Create an object for each supported wiki
-	supported_wikis = {
-		'wikidatawiki': Wikidata( config.get( 'wiki', 'user' ), config.get( 'wiki', 'password' ) ),
-		'frwiktionary': FrWiktionary( config.get( 'wiki', 'user' ), config.get( 'wiki', 'password' ) )
-	}
+def live_mode(args, supported_wikis):
+    #TODO: do stuff
+    return
 
-	# Declare the command-line arguments
-	parser = argparse.ArgumentParser(description='Reuse records made on Lingua Libre on some wikis.')
-	parser.add_argument('--item', help='run only on the given item')
-	parser.add_argument('--startdate', help='from which timestamp to start')
-	parser.add_argument('--enddate', help='at which timestamp to end')
-	parser.add_argument('--user', help='run only on records from the given user')
-	parser.add_argument('--wiki', help='run only on the selected wiki', choices=list( supported_wikis.keys() ))
-	langgroup = parser.add_mutually_exclusive_group()
-	langgroup.add_argument('--lang', help='run only on records from the given language, identified by its lingua libre qid')
-	langgroup.add_argument('--langiso', help='run only on records from the given language, identified by its iso 693-3 code')
-	langgroup.add_argument('--langwm', help='run only on records from the given language, identified by its wikimedia code')
 
-	# Parse the command-line arguments
-	args = parser.parse_args()
-
+def simple_mode(args, supported_wikis):
 	# Add some filters depending on the fetched arguments
 	filters = ""
 	if args.item != None:
@@ -126,10 +109,6 @@ def main():
 	# Get the informations of all the records
 	records = get_records( BASEQUERY.replace( '#filters', filters ) )
 
-	# Filter the wikis depending on the fetched arguments
-	if args.wiki != None:
-		tmp = supported_wikis[ args.wiki ]
-		supported_wikis = { args.wiki: tmp }
 
 	# Prepare the records (fetch extra infos, clean some datas,...)
 	for dbname in supported_wikis:
@@ -142,6 +121,47 @@ def main():
 				time.sleep(1)
 
 	print(len(records))
+
+
+# Main
+def main():
+	# Create an object for each supported wiki
+	supported_wikis = {
+		'wikidatawiki': Wikidata( config.get( 'wiki', 'user' ), config.get( 'wiki', 'password' ) ),
+		'frwiktionary': FrWiktionary( config.get( 'wiki', 'user' ), config.get( 'wiki', 'password' ) )
+	}
+
+	# Declare the command-line arguments
+	parser = argparse.ArgumentParser(description='Reuse records made on Lingua Libre on some wikis.')
+	parser.add_argument('--wiki', help='run only on the selected wiki', choices=list( supported_wikis.keys() ))
+	subparsers = parser.add_subparsers(title='Execution modes')
+
+	liveparser = subparsers.add_parser('live', help='Run llbot in (hardly) real time based on Recent Changes.')
+	liveparser.set_defaults(func=live_mode)
+
+	simpleparser = subparsers.add_parser('simple', help='Run llbot on (a subset of) all items.')
+	simpleparser.set_defaults(func=simple_mode)
+
+	simpleparser.add_argument('--item', help='run only on the given item')
+	simpleparser.add_argument('--startdate', help='from which timestamp to start')
+	simpleparser.add_argument('--enddate', help='at which timestamp to end')
+	simpleparser.add_argument('--user', help='run only on records from the given user')
+	langgroup = simpleparser.add_mutually_exclusive_group()
+	langgroup.add_argument('--lang', help='run only on records from the given language, identified by its lingua libre qid')
+	langgroup.add_argument('--langiso', help='run only on records from the given language, identified by its iso 693-3 code')
+	langgroup.add_argument('--langwm', help='run only on records from the given language, identified by its wikimedia code')
+
+	# Parse the command-line arguments
+	args = parser.parse_args()
+
+
+	# Filter the wikis depending on the fetched arguments
+	if args.wiki != None:
+		tmp = supported_wikis[ args.wiki ]
+		supported_wikis = { args.wiki: tmp }
+
+    # Start the bot in the selected mode (simple or live)
+	args.func(args, supported_wikis)
 
 
 
