fe25f526096a5a53c43c16442ee5b7e957b9fc61
pamputt <pamputt@users.noreply.github.com>
2023-04-05 17:20:48 +0200

    Fix for T274511 (#22)
    * new script to process all recordings
    
    * list of Qidentifiers for all languages supported
    
    * Seems better (#23)
    
    * Improve script (fix some bugs)
    
    * typo
    
    ---------
    
    Co-authored-by: Lopez Hugo <hugo.lpz@gmail.com>

---
+++ scripts/all-languages-llbot.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+export PYTHONUNBUFFERED=1
+# Given a list of languages supported in Lingua Libre (list_languages.txt),
+# and a wiki-specific script name (${wiki_project}),
+# adds missing recording to that targeted wiki.
+
+# Useful when a new wiki project is supported by
+# Lingua Libre Bot in order to add all audio that have
+# been recorded in the past
+
+# Define target project
+wiki_project="kuwiktionary"
+
+# Loop on languages supported by lingualibre
+while read langcode; do
+#    if [ "${langcode}" != "Q21" ]; then
+#       continue
+#    fi
+    
+    echo "Processing ${langcode}"
+    if [ "${langcode}" = "Q21" ] ||      # French
+           [ "${langcode}" = "Q298" ] || # Polish
+           [ "${langcode}" = "Q307" ];   # Bengali
+    then
+        # French language has too many recordings
+        # so splitting the request is required
+        # we ask for recordings for every year
+        current_year=`date +%Y`
+        for year in `seq 2017 ${current_year}`; do
+            for month in 01 07; do
+                startdate="${year}-${month}-01T00:00:00.000+00:00"
+		
+		month_end="01"
+		if [ ${month} = "01" ]; then
+		    month_end="07"
+		fi
+		year_end=${year}
+		if [ ${month_end} = "01" ]; then
+                    year_end=$((${year}+1))
+		fi
+		   
+                enddate="${year_end}-${month_end}-01T00:00:00.000+00:00"
+                echo "start: ${startdate}, end: ${enddate}"
+               $HOME/venv/bin/python3 -u $HOME/Lingua-Libre-Bot/llbot.py --wiki ${wiki_project} simple --lang ${langcode} --startdate ${startdate} --enddate ${enddate}
+		# sleep 5 seconds to avoid Error 429 Too Many Requests
+		sleep 5
+            done
+        done
+    else
+        $HOME/venv/bin/python3 -u $HOME/Lingua-Libre-Bot/llbot.py --wiki ${wiki_project} simple --lang ${langcode}
+    fi
+    
+    # sleep 5 seconds to avoid Error 429 Too Many Requests
+    sleep 5
+done <list_languages.txt
+echo "DONE"
+
